d497f97f88f2a673bc9a2a60d78da20938bbfe6c
Gagan Biradar <Gaganub@users.noreply.github.com>
2026-07-23 11:48:04 +0530

    Initial commit - Monte Carlo simulation model for sports prediction

---
+++ Monte-Carlo-simulation-model-master/evidence/pages/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import './app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} 
+	xProfile="https://www.x.com/Gaganub"
+	blueskyProfile="https://bsky.app/profile/Gaganub"
+	githubRepo="https://www.github.com/Gaganub/nba-monte-carlo"
+	title="mdsinabox"
+	builtWithEvidence=true>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
